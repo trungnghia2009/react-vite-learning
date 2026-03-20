0833b4e84739b7b35332feb42ed41b4c77f32d72
trantrungnghia <nghia.trantrung3@vn.bosch.com>
2026-03-20 16:04:52 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,21 @@
+// Fragmented code, do not edit
+
+import "./style.css";
+
 const MyComponent = () => {
-  return <div>My first Component</div>;
+  return (
+    <>
+      <div
+        className="parent"
+        style={{
+          color: "yellow",
+        }}
+      >
+        My first Component
+      </div>
+      <div className="child">My first Component 2</div>
+    </>
+  );
 };
 
 export default MyComponent;
