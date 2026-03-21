21df0dc1d26f4ef0ef88cfd2eb951ca69a65a908
trantrungnghia <nghia.trantrung3@vn.bosch.com>
2026-03-21 18:48:02 +0700

    video 52

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,15 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/login.jsx";
 import RegisterPage from "./pages/register.jsx";
 import UsersPage from "./pages/users.jsx";
-import ProductsPage from "./pages/products.jsx";
+import BookPage from "./pages/book.jsx";
 import TodoApp from "./components/todo/TodoApp.jsx";
+import ErrorPage from "./pages/error.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -24,8 +26,8 @@ const router = createBrowserRouter([
         element: <UsersPage />,
       },
       {
-        path: "/products",
-        element: <ProductsPage />,
+        path: "/books",
+        element: <BookPage />,
       },
     ],
   },
---
+++ src/pages/book.jsx
@@ -0,0 +1,5 @@
+const BookPage = () => {
+  return <div>Book Pages</div>;
+};
+
+export default BookPage;
