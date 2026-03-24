884e3f62ddaacfa2d2370fededf538c2872f489b
trantrungnghia <nghia.trantrung3@vn.bosch.com>
2026-03-24 15:54:11 +0700

    video 83

--- src/components/user/user.info.jsx
+++ src/components/user/user.info.jsx
@@ -1,11 +1,33 @@
 import { Drawer } from "antd";
+import { useState } from "react";
 
 const UserInfo = (props) => {
   const { userInfo, setOpenDrawer, openDrawer } = props;
 
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [preview, setPreview] = useState(null);
+
   const onClose = () => {
     setOpenDrawer(false);
   };
+
+  const handleUploadAvatar = (event) => {
+    // Check if a file was selected
+    if (!event.target.files || event.target.files.length === 0) {
+      selectedFile(null);
+      setPreview(null);
+      return;
+    }
+
+    const file = event.target.files[0];
+    if (file) {
+      // Handle file upload logic here, e.g., send to backend
+      setSelectedFile(file);
+      setPreview(URL.createObjectURL(file));
+    }
+  };
+
+  console.log("Preview URL:", preview);
 
   return (
     <Drawer
@@ -23,11 +45,20 @@ const UserInfo = (props) => {
 
         <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
           <p style={{ fontWeight: "bold" }}>Avatar</p>
-          <img
-            width={150}
-            src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${userInfo?.avatar}`}
-            alt="User Avatar"
-          />
+          <div
+            style={{
+              height: "100px",
+              width: "150px",
+              border: "1px solid #ccc",
+            }}
+          >
+            <img
+              style={{ height: "100%", width: "100%", objectFit: "contain" }}
+              src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${userInfo?.avatar}`}
+              alt="User Avatar"
+            />
+          </div>
+
           <div>
             <label
               htmlFor="upload-btn"
@@ -42,8 +73,28 @@ const UserInfo = (props) => {
             >
               Upload Avatar
             </label>
-            <input type="file" hidden id="upload-btn" />
+            <input
+              type="file"
+              hidden
+              id="upload-btn"
+              onChange={handleUploadAvatar}
+            />
           </div>
+          {/* Preview of the uploaded avatar */}
+          {preview && (
+            <div
+              style={{
+                width: "150px",
+                border: "1px solid #ccc",
+              }}
+            >
+              <img
+                style={{ height: "100%", width: "100%", objectFit: "contain" }}
+                src={preview}
+                alt="User Avatar"
+              />
+            </div>
+          )}
         </div>
       </div>
     </Drawer>
