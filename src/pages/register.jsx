44fd821c651249487e7a6e624b4e666b997150b0
trantrungnghia <nghia.trantrung3@vn.bosch.com>
2026-03-25 15:25:17 +0700

    video 91

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,4 +1,4 @@
-import { Button, Input, Form, notification } from "antd";
+import { Button, Input, Form, notification, Row, Col } from "antd";
 import { registerUserAPI } from "../services/api.service";
 import { useNavigate } from "react-router-dom";
 
@@ -31,57 +31,72 @@ const RegisterPage = () => {
 
   return (
     <Form
+      style={{ margin: "10px", justifyContent: "center" }}
       form={form}
       layout="vertical"
       name="basic"
       onFinish={onFinish}
-      // onFinishFailed={onFinishFailed}
     >
-      <div
-        style={{
-          margin: "50px",
-        }}
-      >
-        <Form.Item
-          label="Full Name"
-          name="fullName"
-          rules={[{ required: true, message: "Please input your full name!" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[{ required: true, message: "Please input your email!" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item
-          label="Phone Number"
-          name="phone"
-          rules={[
-            {
-              required: true,
-              pattern: /^\d{9,}$/,
-              message: "Phone number must be at least 9 digits!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <div>
+      <Row justify={"center"}>
+        <Col xs={24} md={8}>
+          <Form.Item
+            label="Full Name"
+            name="fullName"
+            rules={[
+              { required: true, message: "Please input your full name!" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row justify={"center"}>
+        <Col xs={24} md={8}>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row justify={"center"}>
+        <Col xs={24} md={8}>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row justify={"center"}>
+        <Col xs={24} md={8}>
+          <Form.Item
+            label="Phone Number"
+            name="phone"
+            rules={[
+              {
+                required: true,
+                pattern: /^\d{9,}$/,
+                message: "Phone number must be at least 9 digits!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs={24} md={8}>
           <Button type="primary" htmlType="submit">
             Register
           </Button>
-        </div>
-      </div>
+        </Col>
+      </Row>
     </Form>
   );
 };
